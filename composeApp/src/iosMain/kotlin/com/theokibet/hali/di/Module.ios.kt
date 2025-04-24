8906aaccb37031cc58bee60e36ffee057de5208e
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 10:59:04 +0300

    setup http clients for android and ios

--- composeApp/src/iosMain/kotlin/com/theokibet/hali/di/Module.ios.kt
+++ composeApp/src/iosMain/kotlin/com/theokibet/hali/di/Module.ios.kt
@@ -1,8 +1,10 @@
 package com.theokibet.hali.di
 
+import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module =
     module {
+        single { Darwin.create() }
     }
