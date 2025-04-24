eb7bab6e56a75811f4ae975deb48636f1863f286
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 05:31:35 +0300

    setup http client

---
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/data/network/client/HaliHttpClient.kt
@@ -0,0 +1,32 @@
+package com.theokibet.hali.data.network.client
+
+import com.theokibet.hali.utils.BASE_URL
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+fun createHttpClient(engine: HttpClientEngine) = HttpClient(engine) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                isLenient = true
+                ignoreUnknownKeys = true
+            }
+        )
+    }
+
+    install(DefaultRequest) {
+        url {
+            host = BASE_URL
+            protocol = URLProtocol.HTTPS
+        }
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+}
