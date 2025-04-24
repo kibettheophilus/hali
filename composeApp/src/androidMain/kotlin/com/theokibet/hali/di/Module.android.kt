d517c9147451aac314281781076f9e68717d2ae2
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 10:59:04 +0300

    setup http clients for android and ios

--- composeApp/src/androidMain/kotlin/com/theokibet/hali/di/Module.android.kt
+++ composeApp/src/androidMain/kotlin/com/theokibet/hali/di/Module.android.kt
@@ -1,8 +1,10 @@
 package com.theokibet.hali.di
 
+import io.ktor.client.engine.android.Android
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual fun platformModule(): Module =
     module {
+        single { Android.create() }
     }
