c7878bc0eca44afda5a58d32a9dd4c87697f3144
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 11:02:17 +0300

    create HomeUiState

---
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeUiState.kt
@@ -0,0 +1,10 @@
+package com.theokibet.hali.ui.screens.home
+
+import com.theokibet.hali.data.models.DailyResponse
+
+sealed interface HomeUiState {
+    data object Default : HomeUiState
+    data object Loading : HomeUiState
+    data class Success(val forecast: DailyResponse) : HomeUiState
+    data class Error(val errorMessage: String) : HomeUiState
+}
