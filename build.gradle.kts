2b90c6f0e4990684ed23c1b7090ea17ae077069d
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 05:46:06 +0300

    setup ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -6,4 +8,22 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        reporters {
+            reporter(ReporterType.JSON)
+        }
+        filter {
+            exclude {
+                projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated/")
+            }
+            exclude {
+                projectDir.toURI().relativize(it.file.toURI()).path.contains("/build/")
+            }
+        }
+    }
 }
