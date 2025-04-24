6646cb6b01054b47675c7d060ae426553d79ffb0
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 11:03:21 +0300

    setup repo

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/data/repositories/WeatherRepository.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/data/repositories/WeatherRepository.kt
@@ -1,7 +1,33 @@
 package com.theokibet.hali.data.repositories
 
+import com.theokibet.hali.data.models.DailyResponse
+import com.theokibet.hali.data.models.HourlyResponse
 import com.theokibet.hali.data.network.api.WeatherApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
-interface WeatherRepository
+interface WeatherRepository {
+    suspend fun getDailyForecast(): Flow<Result<DailyResponse>>
 
-class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherRepository
+    suspend fun getHourlyForecast(): Flow<Result<HourlyResponse>>
+}
+
+class WeatherRepositoryImpl(private val weatherApi: WeatherApi) : WeatherRepository {
+    override suspend fun getDailyForecast(): Flow<Result<DailyResponse>> {
+        return try {
+            val response = weatherApi.getDailyForecast()
+            flowOf(Result.success(response))
+        } catch (exception: Exception) {
+            flowOf(Result.failure(exception))
+        }
+    }
+
+    override suspend fun getHourlyForecast(): Flow<Result<HourlyResponse>> {
+        return try {
+            val response = weatherApi.getHourlyForecast()
+            flowOf(Result.success(response))
+        } catch (exception: Exception) {
+            flowOf(Result.failure(exception))
+        }
+    }
+}
