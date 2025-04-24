15a134d303118b4d704a254451eddee3ee6dbaf4
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 05:30:11 +0300

    move main activity to its package

--- composeApp/src/androidMain/kotlin/com/theokibet/hali/ui/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/theokibet/hali/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.theokibet.hali
+package com.theokibet.hali.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.theokibet.hali.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
