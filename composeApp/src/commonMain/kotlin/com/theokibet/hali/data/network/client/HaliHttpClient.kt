c5513feb6d5bb02916999d54bca7c0570c05fd00
theophiluskibet <kibettheophilus@gmail.com>
2025-04-24 11:01:56 +0300

    add logging to http client

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/data/network/client/HaliHttpClient.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/data/network/client/HaliHttpClient.kt
@@ -1,10 +1,15 @@
 package com.theokibet.hali.data.network.client
 
 import com.theokibet.hali.utils.BASE_URL
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
@@ -21,6 +26,17 @@ fun createHttpClient(engine: HttpClientEngine) =
                     ignoreUnknownKeys = true
                 },
             )
+        }
+
+        install(Logging) {
+            level = LogLevel.HEADERS
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Napier.i(tag = "Http Client", message = message)
+                }
+            }
+        }.also {
+            Napier.base(DebugAntilog())
         }
 
         install(DefaultRequest) {
