9f45c64c8d817274498a187fa1e8e74a7388a142
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:08:21 +0300

    setup DetailViewModel

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailViewModel.kt
@@ -1,5 +1,31 @@
 package com.theokibet.hali.ui.screens.details
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.theokibet.hali.domain.repositories.WeatherRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class DetailViewModel : ViewModel()
+class DetailViewModel(private val weatherRepository: WeatherRepository) : ViewModel(){
+    private val _detailUiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val detailUiState: StateFlow<DetailUiState> = _detailUiState.asStateFlow()
+
+    fun getHourlyForecast(date: String){
+        viewModelScope.launch {
+            weatherRepository.getHourlyForecast(date = date).collect{result ->
+                result.onSuccess { forecast->
+                    _detailUiState.update {
+                        DetailUiState.Success(hourlyForecast = forecast)
+                    }
+                }.onFailure { error->
+                    _detailUiState.update {
+                        DetailUiState.Error(errorMessage = error.message ?: "Uknown error occured")
+                    }
+                }
+            }
+        }
+    }
+}
