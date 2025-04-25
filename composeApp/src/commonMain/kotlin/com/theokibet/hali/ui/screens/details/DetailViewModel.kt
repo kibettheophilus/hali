e7297dd36ac6ac2303408061e44c6daa698188f1
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 13:55:24 +0300

    add selected hour functionality

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailViewModel.kt
@@ -2,6 +2,7 @@ package com.theokibet.hali.ui.screens.details
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.theokibet.hali.domain.models.HourlyForecast
 import com.theokibet.hali.domain.repositories.WeatherRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -9,23 +10,38 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class DetailViewModel(private val weatherRepository: WeatherRepository) : ViewModel(){
+class DetailViewModel(private val weatherRepository: WeatherRepository) : ViewModel() {
     private val _detailUiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
     val detailUiState: StateFlow<DetailUiState> = _detailUiState.asStateFlow()
 
-    fun getHourlyForecast(date: String){
+    fun getHourlyForecast(date: String) {
         viewModelScope.launch {
-            weatherRepository.getHourlyForecast(date = date).collect{result ->
-                result.onSuccess { forecast->
+            weatherRepository.getHourlyForecast(date = date).collect { result ->
+                result.onSuccess { forecast ->
                     _detailUiState.update {
-                        DetailUiState.Success(hourlyForecast = forecast)
+                        DetailUiState.Success(
+                            hourlyForecast = forecast,
+                            selectedHour = forecast.first(),
+                        )
                     }
-                }.onFailure { error->
+                }.onFailure { error ->
                     _detailUiState.update {
                         DetailUiState.Error(errorMessage = error.message ?: "Uknown error occured")
                     }
                 }
             }
+        }
+    }
+
+    fun update(
+        time: HourlyForecast,
+        forecast: List<HourlyForecast>,
+    ) {
+        _detailUiState.update {
+            DetailUiState.Success(
+                hourlyForecast = forecast,
+                selectedHour = time,
+            )
         }
     }
 }
