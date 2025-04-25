668d7672de8ef1c9739188c905be6f80e03f983b
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 14:35:15 +0300

    add junit and ktor mock deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,6 +61,8 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
             implementation(libs.coroutines.test)
+            implementation(libs.ktor.client.mock)
+            implementation(libs.kotlin.test.junit)
         }
 
         androidMain.dependencies {
