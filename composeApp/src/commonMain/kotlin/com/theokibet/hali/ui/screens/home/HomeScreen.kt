3ed767e24e3efafd79127c32ec5c8368bfb3ca46
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 13:54:38 +0300

    update home screen

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeScreen.kt
@@ -1,82 +1,92 @@
 package com.theokibet.hali.ui.screens.home
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.theokibet.hali.ui.components.DailyWeatherCard
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier,
     homeViewModel: HomeViewModel = koinViewModel(),
-    onClick: (String) -> Unit
+    onCardClicked: (String) -> Unit,
 ) {
     val homeUiState by homeViewModel.homeUiState.collectAsState()
-    HomeScreenContent(homeUiState = homeUiState, onClick = onClick)
+
+    HomeScreenContent(
+        homeUiState = homeUiState,
+        onCardClicked = onCardClicked,
+    )
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreenContent(
-    modifier: Modifier = Modifier,
     homeUiState: HomeUiState,
-    onClick: (String) -> Unit
+    onCardClicked: (String) -> Unit,
 ) {
-    when (homeUiState) {
-        HomeUiState.Loading -> {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                CircularProgressIndicator()
-            }
-        }
-
-        is HomeUiState.Error -> {
-            Column(
-                modifier = Modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(homeUiState.errorMessage)
-            }
-        }
-
-        is HomeUiState.Success -> {
-            LazyColumn(
-                modifier = Modifier.padding(5.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                items(homeUiState.dailyForecast) { item ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight(),
-                        onClick = { onClick(item.date) },
-                        backgroundColor = Color.Gray
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Hali")
+                },
+            )
+        },
+    ) { innerPadding ->
+        AnimatedContent(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+            targetState = homeUiState,
+        ) { result ->
+            when (result) {
+                HomeUiState.Loading -> {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
-                        Column(modifier = Modifier.fillMaxSize()) {
-                            Text(item.date)
-                            Text(item.weatherCode.toString())
-                            Text(item.maxTemp.toString())
-                            Text(item.minTemp.toString())
+                        CircularProgressIndicator()
+                    }
+                }
+
+                is HomeUiState.Error -> {
+                    Column(
+                        modifier = Modifier,
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(result.errorMessage)
+                    }
+                }
+
+                is HomeUiState.Success -> {
+                    LazyColumn {
+                        items(result.dailyForecast) { item ->
+                            DailyWeatherCard(
+                                modifier =
+                                    Modifier.fillMaxWidth()
+                                        .padding(horizontal = 16.dp)
+                                        .padding(top = 16.dp),
+                                dailyForecast = item,
+                                onCardClicked = onCardClicked,
+                            )
                         }
                     }
                 }
