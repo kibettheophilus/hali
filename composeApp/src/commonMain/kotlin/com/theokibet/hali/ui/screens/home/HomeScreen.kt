23013cbc4b6fdbed2d73500ec06a6cd7c149a043
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:09:19 +0300

    update home screen

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeScreen.kt
@@ -2,26 +2,51 @@ package com.theokibet.hali.ui.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     homeViewModel: HomeViewModel = koinViewModel(),
+    onClick: (String) -> Unit
 ) {
-    val state by homeViewModel.homeUiState.collectAsState()
+    val homeUiState by homeViewModel.homeUiState.collectAsState()
+    HomeScreenContent(homeUiState = homeUiState, onClick = onClick)
+}
 
-    when (state) {
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun HomeScreenContent(
+    modifier: Modifier = Modifier,
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit
+) {
+    when (homeUiState) {
         HomeUiState.Loading -> {
-        }
-
-        HomeUiState.Default -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                CircularProgressIndicator()
+            }
         }
 
         is HomeUiState.Error -> {
@@ -30,17 +55,31 @@ fun HomeScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text((state as HomeUiState.Error).errorMessage)
+                Text(homeUiState.errorMessage)
             }
         }
 
         is HomeUiState.Success -> {
-            Column(
-                modifier = Modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
+            LazyColumn(
+                modifier = Modifier.padding(5.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
-                Text("${(state as HomeUiState.Success).forecast}")
+                items(homeUiState.dailyForecast) { item ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        onClick = { onClick(item.date) },
+                        backgroundColor = Color.Gray
+                    ) {
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            Text(item.date)
+                            Text(item.weatherCode.toString())
+                            Text(item.maxTemp.toString())
+                            Text(item.minTemp.toString())
+                        }
+                    }
+                }
             }
         }
     }
