f2665435f3ec2b078ac4e8b6eca3e0dd76f0a6ae
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:05:35 +0300

    create detail ui state

---
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailUiState.kt
@@ -0,0 +1,9 @@
+package com.theokibet.hali.ui.screens.details
+
+import com.theokibet.hali.domain.models.HourlyForecast
+
+sealed interface DetailUiState {
+    data object Loading : DetailUiState
+    data class Error(val errorMessage: String): DetailUiState
+    data class Success(val hourlyForecast: List<HourlyForecast>) : DetailUiState
+}
