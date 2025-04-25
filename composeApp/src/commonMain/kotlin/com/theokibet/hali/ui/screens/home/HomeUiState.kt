8f99296198e63c8b47d822301560cd551c3665a5
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 13:55:34 +0300

    lint

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeUiState.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeUiState.kt
@@ -4,6 +4,8 @@ import com.theokibet.hali.domain.models.DailyForecast
 
 sealed interface HomeUiState {
     data object Loading : HomeUiState
+
     data class Success(val dailyForecast: List<DailyForecast>) : HomeUiState
+
     data class Error(val errorMessage: String) : HomeUiState
 }
