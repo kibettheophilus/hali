83523a4f9b7d48da4f6dab0e231767c028c5d2f6
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:05:20 +0300

    add mappers for dto to domain

---
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/data/mappers/DtoToDomain.kt
@@ -0,0 +1,27 @@
+package com.theokibet.hali.data.mappers
+
+import com.theokibet.hali.data.models.Daily
+import com.theokibet.hali.data.models.Hourly
+import com.theokibet.hali.domain.models.DailyForecast
+import com.theokibet.hali.domain.models.HourlyForecast
+
+fun Daily.mapDailyForecast(): List<DailyForecast> {
+    return this.time.indices.map { index ->
+        DailyForecast(
+            date = this.time[index],
+            weatherCode = this.weatherCode[index],
+            maxTemp = this.temperatureMax[index],
+            minTemp = this.temperatureMin[index],
+        )
+    }
+}
+
+fun Hourly.mapHourlyForecast(): List<HourlyForecast> {
+    return this.time.indices.map { index ->
+        HourlyForecast(
+            precipitation = this.precipitation[index],
+            temperatureM = this.temperatureM[index],
+            time = this.time[index],
+        )
+    }
+}
