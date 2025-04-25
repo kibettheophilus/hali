5250a728fe7fac6f4a6dda3df1a5e703e479efc5
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:06:38 +0300

    setup navigation

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/navigation/HaliNavigation.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/navigation/HaliNavigation.kt
@@ -1,3 +1,32 @@
 package com.theokibet.hali.ui.navigation
 
-class HaliNavigation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.theokibet.hali.ui.screens.details.DetailScreen
+import com.theokibet.hali.ui.screens.home.HomeScreen
+
+@Composable
+fun HaliNavigation(modifier: Modifier = Modifier) {
+    val navController: NavHostController = rememberNavController()
+    NavHost(
+        modifier = Modifier,
+        navController = navController,
+        startDestination = Home
+    ) {
+        composable<Home> {
+            HomeScreen(onClick = { date ->
+                navController.navigate(Details(date = date))
+            })
+        }
+
+        composable<Details> { navBackStackEntry ->
+            val details: Details = navBackStackEntry.toRoute()
+            DetailScreen(date = details.date)
+        }
+    }
+}
