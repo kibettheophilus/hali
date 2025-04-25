6a6add0cc0b863cbe7427a8369ac974f62071d33
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 13:51:02 +0300

    delete unused components

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/components/HaliButton.kt
+++
@@ -1,8 +0,0 @@
-package com.theokibet.hali.ui.components
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun HaliButton(modifier: Modifier = Modifier) {
-}
