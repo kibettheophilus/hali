c11b005162b8bb4bd4a53ba06dd37c86963a13f7
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 08:09:43 +0300

    update home vm

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/home/HomeViewModel.kt
@@ -2,7 +2,7 @@ package com.theokibet.hali.ui.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.theokibet.hali.data.repositories.WeatherRepository
+import com.theokibet.hali.domain.repositories.WeatherRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val weatherRepository: WeatherRepository) : ViewModel() {
-    private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Default)
+    private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
     val homeUiState: StateFlow<HomeUiState> = _homeUiState.asStateFlow()
 
     init {
@@ -20,16 +20,15 @@ class HomeViewModel(private val weatherRepository: WeatherRepository) : ViewMode
 
     fun getDailyForecast() {
         viewModelScope.launch {
-            _homeUiState.update {
-                HomeUiState.Loading
-            }
             weatherRepository.getDailyForecast().collectLatest { result ->
                 result.onSuccess { forecast ->
                     _homeUiState.update {
-                        HomeUiState.Success(forecast = forecast)
+                        HomeUiState.Success(dailyForecast = forecast)
                     }
                 }.onFailure { error ->
-                    HomeUiState.Error(errorMessage = error.message ?: "Unknown error occured")
+                    _homeUiState.update {
+                        HomeUiState.Error(errorMessage = error.message ?: "Unknown error occured")
+                    }
                 }
             }
         }
