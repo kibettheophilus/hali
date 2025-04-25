14d7fac069cfc4a2692ce20433d2525f7fdb8bee
theophiluskibet <kibettheophilus@gmail.com>
2025-04-25 13:55:24 +0300

    add selected hour functionality

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailUiState.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/ui/screens/details/DetailUiState.kt
@@ -4,6 +4,9 @@ import com.theokibet.hali.domain.models.HourlyForecast
 
 sealed interface DetailUiState {
     data object Loading : DetailUiState
-    data class Error(val errorMessage: String): DetailUiState
-    data class Success(val hourlyForecast: List<HourlyForecast>) : DetailUiState
+
+    data class Error(val errorMessage: String) : DetailUiState
+
+    data class Success(val hourlyForecast: List<HourlyForecast>, val selectedHour: HourlyForecast) :
+        DetailUiState
 }
