d5f92f5db1e739798745d954b230c47e5e200db5
theophiluskibet <kibettheophilus@gmail.com>
2025-06-20 00:09:35 +0300

    setup reproducer for koin#2221

--- composeApp/src/commonMain/kotlin/com/theokibet/hali/App.kt
+++ composeApp/src/commonMain/kotlin/com/theokibet/hali/App.kt
@@ -2,21 +2,27 @@ package com.theokibet.hali
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.theokibet.hali.di.sharedModule
 import com.theokibet.hali.ui.navigation.HaliNavigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
+import org.koin.core.module.Module
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    platformModule: Module = Module()
+) {
+    val navController: NavHostController = rememberNavController()
     KoinApplication(
         application = {
-            modules(sharedModule)
+            modules(listOf(sharedModule,platformModule))
         },
     ) {
         MaterialTheme {
-            HaliNavigation()
+            HaliNavigation(navController)
         }
     }
 }
